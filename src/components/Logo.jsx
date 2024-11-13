181ab8ccf690d036de7f123cacedea9849e90a55
sumeshp09~ <sumeshpawar0@gmail.com>
2024-11-13 22:16:12 +0530

    V1

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function Logo ({width = '100px'}){
+
+    return (
+        <div>Logo</div>
+    )       
+
+}
+
+export default Logo
---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import {configureStore} from '@reduxjs/toolkit';
+import authSlice from './authSlice';
+
+const store = configureStore({
+    reducer: {
+        auth : authSlice,
+        // post: postSlice,
+    }
+});
+
+
+export default store;
